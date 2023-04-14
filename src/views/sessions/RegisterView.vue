8a61db51cdc6731247415444285534fa3e1e609b
HeckerCreatives <daniel.janbiengabrielle@gmail.com>
2023-04-14 18:05:53 +0800

    posh

--- src/views/sessions/RegisterView.vue
+++ src/views/sessions/RegisterView.vue
@@ -1,9 +1,25 @@
 <template>
-    <p>hello</p>
+    <div class="register">
+        <RegisterSteps />
+    </div>
 </template>
 
 <script>
+import RegisterSteps from '@/components/sessions/register/RegisterSteps.vue'
+
 export default{
-    name: 'RegisterView'
+    name: 'RegisterView',
+    components: {
+        RegisterSteps
+    }
 }
 </script>
+
+<style>
+.register{
+    width: 100vw;
+    height: 100vh;
+    background-color: aqua;
+    margin: 0;
+}
+</style>
